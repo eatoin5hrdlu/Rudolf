24d8dede9780297683e4cf62a01c5f18e28228f4
Peter Reintjes <peter.reintjes@ncmls.org>
2015-12-05 14:26:24 -0500

    detaching and reattaching interrupts as needed

---
+++ rudolf4/rudolf4.ino
@@ -0,0 +1,344 @@
+//#define DEBUG 1
+//#define digitalPinToInterrupt(p)  (p==2?0:(p==3?1:(p>=18&&p<=21?23-p:-1)))
+// Add capacitance to interrupt input lines (Halfway is triggering early)
+// Self reset after going off the rails.
+// Pulses on output pin keep reset high,
+// if pulses stop, pin goes low, but only for a second
+// need stronger pullup on halfway pin 
+
+// DEBUGGING: keep track of the number of resets
+
+#define DEBUG_PIN 19
+
+#include <EEPROM.h>
+boolean once;
+void debug_incr()
+{
+	int byte = EEPROM.read(0);
+	byte = byte + 1;
+	EEPROM.write(0,byte);
+}
+void debug_zero()
+{
+	EEPROM.write(0,0);
+}
+void debug_initialize()
+{
+	if (once and !digitalRead(DEBUG_PIN))
+	{
+		once = false;
+		Serial.begin(9600);
+		while (!Serial) { ; }
+	}
+}
+void debug_resets()
+{
+	if (!digitalRead(DEBUG_PIN))
+	{
+	   int byte = EEPROM.read(0);
+	   debug_initialize();
+	   Serial.println("");
+	   Serial.print(byte);
+	   Serial.println(" resets");
+	}
+}
+void debug_string(char *str)
+{
+	if (!digitalRead(DEBUG_PIN))
+	{
+	   debug_initialize();
+	   Serial.print(str);
+	}
+}
+void debug_int(int i)
+{
+	if (!digitalRead(DEBUG_PIN))
+	{
+	   debug_initialize();
+	   Serial.print(i);
+	}
+}
+void debug_long(unsigned long l)
+{
+	if (!digitalRead(DEBUG_PIN))
+	{
+	   debug_initialize();
+	   Serial.print(l);
+	}
+}
+		
+#define LED         13
+#define TRAIN        2
+#define HALFWAY     20
+#define FINISHED    21
+
+#define TIMEOUT     180000UL
+
+#define MOTOR       5
+#define DIRECTION   6
+
+#define STARTUP     0
+#define IDLE        1
+#define OUTBOUND    2
+#define PAUSING     3
+#define PAUSED      4
+#define INBOUND     5
+#define ALLDONE     6
+#define RESTING     7
+
+#define LONG_LOCKOUT     160000UL
+#define SHORT_LOCKOUT     80000UL
+#define OUTBOUND_LOCKOUT  55000UL
+
+unsigned long m_lasttime;
+unsigned long d_lasttime;
+
+char *statename[] = {
+	"\nSTARTUP",
+	"\nIDLE",
+	"\nOUTBOUND",
+	"\nPAUSING",
+	"\nPAUSED",
+	"\nINBOUND",
+	"\nALLDONE",
+	"\nRESTING",
+};
+
+boolean direction;
+boolean motorON;
+
+unsigned long int last_timestamp;
+unsigned long int durationMS;
+unsigned long int surgeMS = 1200UL;
+
+unsigned long occ;
+volatile unsigned long int lockout; // Don't re-trigger for a while
+volatile int state;                // What are we doing?
+volatile unsigned long int count;  // Did we get lost somehow?
+volatile boolean train;            // Here comes the Train
+
+void detector() { if (state == IDLE     && lockout == 0) {count=0; train=true; }}
+void halfway()  { if (state == OUTBOUND && lockout == 0) {setstate(PAUSING);   }}
+void finished() { if (state == INBOUND) { lockout=LONG_LOCKOUT; train=false; setstate(ALLDONE);}}
+
+
+void setstate(int value)
+{
+	state = value;
+}
+
+void showstate()
+{
+static int last_state;
+	if (state != last_state)
+		debug_string(statename[state]);
+	last_state = state;
+}
+
+void mydelay(int ms)
+{
+	int msdec = ms/10;
+	while(msdec-- > 0) delayMicroseconds(10000);
+}
+
+void NotSoFast()
+{
+unsigned long now = millis();
+	if ( ( now - m_lasttime < 200) ||  (now - d_lasttime) < 200 )
+	{
+		debug_string("\nToggling motor too fast...");
+		mydelay(1000);
+		debug_string("ok now.\n");
+	}
+}
+
+
+void motor(boolean m)
+{
+	if (m != motorON)
+	{
+		NotSoFast();
+		motorON = m;
+		m_lasttime = millis();
+	}
+	digitalWrite(MOTOR, m);
+}
+
+void set_direction(boolean d)
+{
+	if (d != direction) 
+	{
+		NotSoFast();
+		direction = d;
+		d_lasttime = millis();
+	}
+	digitalWrite(DIRECTION, direction);
+}
+
+void showpins()
+{
+int t,h,f;
+	if (occ % 100000 == 0)
+	{
+		t = digitalRead(TRAIN);
+		h = digitalRead(HALFWAY);
+		f = digitalRead(FINISHED);
+		debug_string("\nlockout "); debug_long(lockout);
+		debug_string(" train "); debug_int(h);
+		debug_string(" halfw "); debug_int(h);
+		debug_string(" finis "); debug_int(f);
+		debug_string("\n");
+	}
+}
+
+void setup() 
+{
+	pinMode(DEBUG_PIN,INPUT_PULLUP);
+	once = true;
+	debug_initialize(); // Conditionally start serial comm
+	debug_incr();
+//	debug_zero();
+	setstate(STARTUP);
+	lockout = LONG_LOCKOUT;  // Cannot trigger immediately after a reset
+
+	pinMode(TRAIN,INPUT_PULLUP);
+	pinMode(HALFWAY,INPUT_PULLUP);
+	pinMode(FINISHED,INPUT_PULLUP);
+
+	pinMode(LED,      OUTPUT);
+	digitalWrite(LED, 0);
+	pinMode(MOTOR,    OUTPUT);
+	motor(false);
+	pinMode(DIRECTION,OUTPUT);
+	set_direction(false);
+	durationMS = 3000UL;
+
+	occ = 0;
+	count = 0;
+
+	train = false;
+	setstate(IDLE);
+	attachInterrupt(digitalPinToInterrupt(TRAIN),detector,FALLING);
+
+	debug_resets();
+	d_lasttime = millis();
+	m_lasttime = millis();
+}
+
+void clearall()
+{
+	setstate(IDLE);
+	count = 0;
+	motor(false);
+	set_direction(false);
+}
+
+boolean phase()
+{
+static boolean last;
+boolean p = !(((millis()-last_timestamp)/surgeMS)%2);
+	if (p != last) debug_int(p);
+	last = p;
+	return(p);
+}
+
+void loop()
+{
+	if (lockout > 0) {
+		lockout--;
+		if (lockout == 0)
+		{	attachInterrupt(digitalPinToInterrupt(TRAIN),detector,FALLING);
+	attachInterrupt(digitalPinToInterrupt(HALFWAY),halfway,FALLING);
+	attachInterrupt(digitalPinToInterrupt(FINISHED),finished,FALLING);
+
+			debug_string("\nUnLocked");
+			if (state == IDLE)  // Now get ready for the train
+			{
+			debug_string("               READY");
+			train = false;
+			attachInterrupt(digitalPinToInterrupt(TRAIN),detector,FALLING);
+			clearall();
+			}
+		}
+	}
+
+		occ++;
+		showstate();
+		showpins();
+		switch(state) {
+			case INBOUND:
+				detachInterrupt(digitalPinToInterrupt(HALFWAY));
+				set_direction(true);
+				motor(phase());
+				if (count++ > TIMEOUT) {
+					set_direction(false);
+					debug_string("\nINbound timeout\n");
+					setstate(ALLDONE);
+				}
+				break;
+			case OUTBOUND:
+				set_direction(false);
+				motor(phase());
+				if (count++ > TIMEOUT)  //Outbound switch fail
+				{
+					motor(false);
+					set_direction(true);
+					debug_string("\nOUTbound timeout\n");
+					setstate(PAUSING);
+				}
+				break;
+			case PAUSING:
+				detachInterrupt(digitalPinToInterrupt(HALFWAY));
+				count = 0;
+				motor(false);
+				set_direction(true);
+				last_timestamp = millis();
+				durationMS = 2000UL;
+				setstate(PAUSED);
+				break;
+			case PAUSED:
+				count = 0;
+				if (millis() > last_timestamp + durationMS)
+				{
+					set_direction(true);
+					attachInterrupt(digitalPinToInterrupt(FINISHED),finished,FALLING);
+					setstate(INBOUND);
+				}
+				break;
+			case IDLE:
+				digitalWrite(LED,phase());
+				if (train) {
+					detachInterrupt(digitalPinToInterrupt(TRAIN));
+					set_direction(false);
+					last_timestamp = millis();
+					setstate(OUTBOUND);
+					lockout = OUTBOUND_LOCKOUT;
+					attachInterrupt(digitalPinToInterrupt(HALFWAY),halfway,FALLING);
+					count = 0;
+					train = false;
+				} else
+					clearall();
+				break;
+			case ALLDONE:
+				detachInterrupt(digitalPinToInterrupt(FINISHED));
+				count = 0;
+				motor(false);
+				last_timestamp = millis();
+				durationMS = 2000UL;
+				setstate(RESTING);
+				break;
+			case RESTING:
+				lockout = LONG_LOCKOUT;
+				if (millis() > last_timestamp + durationMS) {
+					attachInterrupt(digitalPinToInterrupt(TRAIN),detector,FALLING);
+					setstate(IDLE);
+				}
+				break;
+			default:
+				debug_string("This shouldn't happen!");
+				setup();//??
+				break;
+		} // End Switch
+		phase();
+//	} // End (Optional) while(1)
+} // End Loop
